c8652d2ac4a2529e022e420dbfb80cfe48477ece
Daniil Shreyder <daniil.schroder@gmail.com>
2023-11-18 04:37:26 +0300

    app

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::panic::panic_any;
+
+fn main() {
+    println!("{}", get_primes(100).into_iter()
+        .fold(
+            String::new(),
+            |a: String, b: String | a.clone() // i don't yet understand why, but sure
+                + (if !a.is_empty() && !b.is_empty() { ", " } else { "" })
+                + &b
+        )
+    )
+}
+
+fn get_primes<'a>(nums: usize) -> Vec<String> {
+
+    let mut board = vec![true; nums];
+    let mut primes = Vec::new();
+
+    for i in 2..=nums {
+        match board.get(i) {
+            Some(prime) if !prime => continue,
+            Some(prime) if *prime => {
+                primes.insert(primes.len(), i.to_string());
+
+                for not_prime in (i + 1..nums).step_by(i)  {
+                    board.insert(not_prime, false);
+                }
+            }
+            _ => panic_any("Not possible"),
+        }
+    }
+
+    primes
+}
